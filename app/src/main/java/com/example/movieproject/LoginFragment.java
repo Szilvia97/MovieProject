6fad9f6c5df88da0d53d5e39f3bf3dc4bf824db1
Szilvia97 <szilvia_krisz_tina@yahoo.com>
2020-01-03 18:56:31 +0200

    Data saved in db

--- app/src/main/java/com/example/movieproject/LoginFragment.java
+++ app/src/main/java/com/example/movieproject/LoginFragment.java
@@ -3,22 +3,101 @@ package com.example.movieproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.movieproject.Model.User;
 
 
 public class LoginFragment extends Fragment {
 
+    EditText et_login_pwd, et_login_email;
+    TextView tv_reg;
+    Button btn_login;
+    SqliteHelper sqliteHelper;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        et_login_email = v.findViewById(R.id.et_login_email);
+        et_login_pwd = v.findViewById(R.id.et_login_pwd);
+        tv_reg = v.findViewById(R.id.tv_reg);
+        btn_login = v.findViewById(R.id.btn_login);
+
+        sqliteHelper = new SqliteHelper(getContext());
+
+        btn_login.setOnClickListener(onClickListenerLogin);
+
+        return v;
     }
 
+    private View.OnClickListener onClickListenerLogin = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            sqliteHelper = new SqliteHelper(getContext());
+            if (validate()) {
 
+                String Email = et_login_email.getText().toString();
+                String Password = et_login_pwd.getText().toString();
+
+                User currentUser = sqliteHelper.Authenticate(new User(null, null, Email, Password));
+
+                if (currentUser != null) {
+                    Toast.makeText(getContext(), "Successfully Logged in!", Toast.LENGTH_LONG).show();
+                    Fragment fragment = new HomeFragment();
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_id, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+
+                } else {
+                    Toast.makeText(getContext(), "Failed to log in , please try again", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        }
+    };
+    public boolean validate() {
+        boolean valid = false;
+
+        String Email = et_login_email.getText().toString();
+        String Password = et_login_pwd.getText().toString();
+
+        //Handling validation for Email field
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
+            valid = false;
+            et_login_email.setError("Please enter valid email!");
+        } else {
+            valid = true;
+            et_login_email.setError(null);
+        }
+
+        //Handling validation for Password field
+        if (Password.isEmpty()) {
+            valid = false;
+            et_login_pwd.setError("Please enter valid password!");
+        } else {
+            if (Password.length() > 5) {
+                valid = true;
+                et_login_pwd.setError(null);
+            } else {
+                valid = false;
+                et_login_pwd.setError("Password is to short!");
+            }
+        }
+
+        return valid;
+    }
 }
--- app/src/main/java/com/example/movieproject/RegisterFragment.java
+++ app/src/main/java/com/example/movieproject/RegisterFragment.java
@@ -3,13 +3,20 @@ package com.example.movieproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Handler;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.movieproject.Model.User;
 
 
 public class RegisterFragment extends Fragment {
@@ -17,6 +24,7 @@ public class RegisterFragment extends Fragment {
     EditText et_reg_pwd, et_reg_email, et_reg_username;
     TextView tv_login;
     Button btn_reg;
+    SqliteHelper sqliteHelper;
 
 
     @Override
@@ -30,9 +38,89 @@ public class RegisterFragment extends Fragment {
         tv_login = v.findViewById(R.id.tv_login);
         btn_reg = v.findViewById(R.id.btn_reg);
 
+        sqliteHelper = new SqliteHelper(getContext());
+
+        btn_reg.setOnClickListener(onClickListenerRegister);
+
         return v;
 
     }
 
+    private View.OnClickListener onClickListenerRegister = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            sqliteHelper = new SqliteHelper(getContext());
 
+            if (validate()) {
+                String UserName = et_reg_username.getText().toString();
+                String Email = et_reg_email.getText().toString();
+                String Password = et_reg_pwd.getText().toString();
+
+                if (!sqliteHelper.isEmailExists(Email)) {
+
+                    sqliteHelper.addUser(new User(null, UserName, Email, Password));
+                    Toast.makeText(getContext(), "User created successfully! Please Login ", Toast.LENGTH_LONG).show();
+
+                    Fragment fragment = new LoginFragment();
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_id, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+
+                } else {
+
+                    Toast.makeText(getContext(), "User already exists with same email ", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        }
+    };
+
+
+
+    public boolean validate() {
+        boolean valid = false;
+
+        String UserName = et_reg_username.getText().toString();
+        String Email = et_reg_email.getText().toString();
+        String Password = et_reg_pwd.getText().toString();
+
+        if (UserName.isEmpty()) {
+            valid = false;
+            et_reg_username.setError("Please enter valid username!");
+        } else {
+            if (UserName.length() > 5) {
+                valid = true;
+                et_reg_username.setError(null);
+            } else {
+                valid = false;
+                et_reg_username.setError("Username is to short!");
+            }
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
+            valid = false;
+            et_reg_email.setError("Please enter valid email!");
+        } else {
+            valid = true;
+            et_reg_email.setError(null);
+        }
+
+        if (Password.isEmpty()) {
+            valid = false;
+            et_reg_pwd.setError("Please enter valid password!");
+        } else {
+            if (Password.length() > 5) {
+                valid = true;
+                et_reg_pwd.setError(null);
+            } else {
+                valid = false;
+                et_reg_pwd.setError("Password is to short!");
+            }
+        }
+
+        return valid;
+    }
 }
